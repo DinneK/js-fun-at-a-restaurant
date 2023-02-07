633bafdd97d95077895f15be488079e4ef3caa17
Dinne Kopelevich <dinne22@hotmail.com>
2023-02-07 10:52:30 -0700

    Add meal

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,47 @@
-function nameMenuItem() {
+const nameMenuItem = (menuItemName) => {
+  return `Delicious ${menuItemName}`;
+};
 
-}
+const createMenuItem = (name, price, type) => {
+  const menuItem = {
+    name,
+    price,
+    type,
+  };
+  return menuItem;
+};
 
+const addIngredients = (ingredient, ingredients) => {
+  if (ingredients.includes(ingredient)) {
+    return ingredients.length;
+  } else {
+    ingredients.push(ingredient);
+  }
+};
+
+const formatPrice = (price) => {
+  return `$${price}`;
+};
+
+const decreasePrice = (price) => {
+  const newPrice = price - price * 0.1;
+  return newPrice;
+};
+
+const createRecipe = (title, ingredients, type) => {
+  const recipe = {
+    title,
+    ingredients,
+    type,
+  };
+  return recipe;
+};
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
-}
-
-
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe,
+};
