f09b0db71cacc7a0622a85352d832afc20173d19
Dinne Kopelevich <dinne22@hotmail.com>
2023-02-07 16:25:33 -0700

    Add order.js

--- src/order.js
+++ src/order.js
@@ -1,7 +1,33 @@
+const takeOrder = (order, deliveryOrders) => {
+  if (deliveryOrders.length < 3) {
+    deliveryOrders.push(order);
+  }
+};
+
+const refundOrder = (num, deliveryOrders) => {
+  deliveryOrders.forEach((order, idx) => {
+    if (order.orderNumber === num) {
+      deliveryOrders.splice(idx, 1);
+    }
+  });
+  return deliveryOrders;
+};
+
+const listItems = (deliveryOrders) => {
+  return deliveryOrders
+    .map((order) => order.item)
+    .toString()
+    .split(",")
+    .join(", ");
+};
+
+const searchOrder = (deliveryOrders, itemName) => {
+  return deliveryOrders.some((order) => order.item === itemName);
+};
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
-}
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder,
+};
