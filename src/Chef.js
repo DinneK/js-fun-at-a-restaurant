b7d5bb69015bd61b4502f361f6fbfbcc53a5ce0f
Dinne Kopelevich <dinne22@hotmail.com>
2023-02-08 18:05:25 -0700

    Add restaurant.js and Chef.js

--- src/Chef.js
+++ src/Chef.js
@@ -1,7 +1,23 @@
 class Chef {
-  constructor() {
-    
+  constructor(name, restaurant) {
+    this.name = name;
+    this.restaurant = restaurant;
   }
+
+  greetCustomer = (name, morning) => {
+    if (morning) {
+      return `Good morning, ${name}!`;
+    }
+    return `Hello, ${name}!`;
+  };
+
+  checkForFood = (food) => {
+    if (!this.restaurant.menus[food.type].length) {
+      return `Sorry, we aren't serving ${food.name} today.`;
+    } else {
+      return `Yes, we're serving ${food.name} today!`;
+    }
+  };
 }
 
 module.exports = Chef;
--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,42 @@
+const createRestaurant = (name) => {
+  const restaurant = {
+    name,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: [],
+    },
+  };
+  return restaurant;
+};
 
+const addMenuItem = (restaurant, item) => {
+  // console.log("restaurants", restaurant.menus[item.type]);
+  // console.log("item", item.type);
+
+  if (item.type === "breakfast" && !restaurant.menus.breakfast.includes(item)) {
+    return restaurant.menus.breakfast.push(item);
+  }
+  if (item.type === "lunch" && !restaurant.menus.lunch.includes(item)) {
+    return restaurant.menus.lunch.push(item);
+  }
+  if (item.type === "dinner" && !restaurant.menus.dinner.includes(item)) {
+    return restaurant.menus.dinner.push(item);
+  }
+};
+
+const removeMenuItem = (menus, item, time) => {
+  if (menus.menus[time].length === 0) {
+    return `Sorry, we don't sell ${item}, try adding a new recipe!`;
+  }
+  if (menus.menus[time][0].type === time) {
+    menus.menus[time].splice(0, 1);
+    return `No one is eating our ${item} - it has been removed from the ${time} menu!`;
+  }
+};
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
-}
+  createRestaurant,
+  addMenuItem,
+  removeMenuItem,
+};
